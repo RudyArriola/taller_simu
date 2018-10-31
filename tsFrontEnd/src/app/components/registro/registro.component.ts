48e909fd60689f9302fd01d5c66c7e32cb8d9f10
Narrieth <narrieth14ajc@gmail.com>
2018-10-31 12:44:54 -0400

    ultimos cambios de forntend

--- tsFrontEnd/src/app/components/registro/registro.component.ts
+++ tsFrontEnd/src/app/components/registro/registro.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {MatTableDataSource} from '@angular/material';
+import {FormBuilder, FormGroup} from '@angular/forms';
 // import {ErrorStateMatcher} from '@angular/material/core';
 // import { MatDialog, MatDialogRef } from '@angular/material';
 
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
+  weight: string;
   symbol: string;
   dem: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', dem: 'ana'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', dem: 'pedro'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', dem: 'juan'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', dem: 'maria'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', dem: 'jonathan'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', dem: 'juan pablo'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', dem: 'paola'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', dem: 'claudia'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', dem: 'fernanda'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', dem: 'diego'},
+  {position: 1, name: 'Jose', weight: 'Juez 1', symbol: 'Acciones Mineras', dem: 'ana'},
+  {position: 2, name: 'Alejandro', weight: 'Juez 2', symbol: 'Actos Municipales', dem: 'pedro'},
+  {position: 3, name: 'Julio', weight: 'Juez 1', symbol: 'Actos Municipales', dem: 'juan'},
+  {position: 4, name: 'Boris', weight: 'Juez 1', symbol: 'Impugnacion por actos administrativos', dem: 'maria'},
+  {position: 5, name: 'Yony', weight: 'Juez 2', symbol: 'Impugnacion de Resoluciones emitidas por el SENASIG', dem: 'jonathan'},
+  {position: 6, name: 'Marcelo', weight: 'Juez 2', symbol: 'Actos Municipales', dem: 'juan pablo'},
+  {position: 7, name: 'Carla', weight: 'Juez 3', symbol: 'Impugnacion por actos administrativos', dem: 'paola'},
+  {position: 8, name: 'Casimiro', weight: 'Juez 3', symbol: 'Actos Municipales', dem: 'claudia'},
+  {position: 9, name: 'Federico', weight: 'Juez 2', symbol: 'Impugnacion de Resoluciones emitidas por el SENASIG', dem: 'fernanda'},
+  {position: 10, name: 'Constancio', weight: 'Juez 1', symbol: 'Impugnacion por actos administrativos', dem: 'diego'},
 ];
-
 
 @Component({
   selector: 'app-registro',
@@ -32,18 +32,24 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-  demandanteFormControl = new FormControl('', [Validators.required]);
-  demandadoFormControl = new FormControl('', [Validators.required]);
-  telefonoFormControl = new FormControl('', [Validators.required]);
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'dem'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  fourthFormGroup: FormGroup;
+
+  demandanteFormControl = new FormControl('', [Validators.required]);
+  demandadoFormControl = new FormControl('', [Validators.required]);
+  telefonoFormControl = new FormControl('', [Validators.required]);
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
   // disabled = false;
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
